2000d63e61b7bcc2ba98040bbd9d56e1682c321a
Steven lopez <cslopezmontano@gmail.com>
2024-05-02 11:36:25 -0500

    Cambios

---
+++ HelloWorld/src/variablesJava.java
@@ -0,0 +1,16 @@
+
+public class variablesJava {
+	public static void main (String [] args) {
+	
+		String saludar = "Welcome :)";
+		String nombrePrueba = "Steven";
+		
+		
+		
+		System.out.println(nombrePrueba);
+		System.out.println(saludar.toUpperCase());
+		System.out.println(nombrePrueba + " " + saludar);
+		
+	}
+	
+}
