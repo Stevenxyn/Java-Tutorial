bc9ba91f3f6d13d5fa1051eecd2922ff1938a4cb
Steven Lopez <cslopezmontano@gmail.com>
2024-05-25 09:58:23 -0500

    estructuras de control de flujos IF

---
+++ HelloWorld/src/Estructuras_De_Control_Flujo/EjemplosIf.java
@@ -0,0 +1,26 @@
+package Estructuras_De_Control_Flujo;
+
+public class EjemplosIf {
+
+	public static void main (String args[]) {
+		
+		int edad = 11;
+		
+		if(edad >= 18) {
+			System.out.println("Puedes ingresar al lugar");
+		} else {
+			System.out.println("No puedes ingresar");
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
