3921f9fdaffbacd2454a2ed4a2aa7f8c55195f5d
Steven Lopez <carlos.lopez-mo@uniminuto.edu.co>
2025-01-12 17:58:28 -0500

    DecrementOperatos

--- JavaProgramming/src/day3/DecrementOperator.java
+++ JavaProgramming/src/day3/DecrementOperator.java
@@ -3,8 +3,25 @@ package day3;
 public class DecrementOperator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
+		int a = 100;
+		a = a - 1;
+		System.out.println(a);
+		
+		
+		//Post decrement
+		int b = 100;
+		int rest = b--;
+		System.out.println(rest); //100
+		
+		//Post decrement
+		int x = 100;
+		int rest2 = --x;
+		System.out.println(rest2); //100
+		
+		rest2--;
+		System.out.println(rest2);
 	}
 
 }
